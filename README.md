85d455637ef63ee92e7258677bcf622e0746014b
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-12-23 11:06:56 -0800

    add project with ut

---
+++ README.md
@@ -0,0 +1,4 @@
+https://mva.microsoft.com/zh-cn/training-courses/net-core--16764?l=DoVafl7yC_7606218965
+
+
+dotnet new -t xunittest
---
+++ test/CharacterCounter.Tests/CharacterCounter.Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+using Library;
+
+namespace CharacterCounterTests
+{
+    public class GetCountForLetter
+    {
+        [Fact]
+        public void should_return_a_count_of_1_if_there_is_a_single_letter_to_parse() 
+        {
+            CharacterCounter counter = new CharacterCounter();
+            counter.ParseString("a");
+
+            Assert.Equal(2, counter.GetCountForLetter('a'));
+        }
+    }
+}
